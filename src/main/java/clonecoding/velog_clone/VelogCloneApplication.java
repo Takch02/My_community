37700ed4f0346d76082aea6b15e91ca1093d76fe
takch02 <lim70kr38@gmail.com>
2025-03-30 17:26:59 +0900

    Add files via upload

--- src/main/java/clonecoding/velog_clone/VelogCloneApplication.java
+++ src/main/java/clonecoding/velog_clone/VelogCloneApplication.java
@@ -1,9 +1,11 @@
 package clonecoding.velog_clone;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("clonecoding.velog_clone.mapper")
 public class VelogCloneApplication {
 
     public static void main(String[] args) {
