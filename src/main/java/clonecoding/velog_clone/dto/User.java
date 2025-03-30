6f1535f81a3e9eedcb1c83495526862899ebc9a1
takch02 <lim70kr38@gmail.com>
2025-03-30 18:10:11 +0900

    Update User.java

--- src/main/java/clonecoding/velog_clone/dto/User.java
+++ src/main/java/clonecoding/velog_clone/dto/User.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.ibatis.type.Alias;
-@Alias("User")
+
 @Getter
 @Setter
 public class User {
